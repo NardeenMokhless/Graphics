bec7e530722e48271c26dab4cbd62ca2c5bd48b8
Nardeen <nardeenmokhless11@gmail.com>
2018-05-15 21:37:44 +0200

    header and resource

---
+++ menu_header.h
@@ -0,0 +1,23 @@
+#ifndef MENU_HEADER_H_INCLUDED
+#define MENU_HEADER_H_INCLUDED
+
+#define MY_MENU_ID 1
+#define MY_FILE_EXIT_ID 2
+#define MY_LINE_MIDPOINT_ID 3
+#define MY_CIRCLE_MIDPOINT_ID 4
+#define MY_LINE_CARTESIAN_ID  5
+#define MY_CIRCLE_CARTESIAN_ID 6
+#define FILL_FLOOD_ID 7
+#define COHEN_ID 8
+#define Parabola_ID 9
+#define MY_LINE_DDA_ID 10
+#define MY_CIRCLE_POLAR_ID 11
+#define POLYGON_ID 12
+#define POLYGON2_ID 13
+#define Cubic_ID 14
+#define Save_ID 15
+#define Load_ID 16
+#define Hermite_ID 17
+#define PolygonClipping_ID 18
+
+#endif
